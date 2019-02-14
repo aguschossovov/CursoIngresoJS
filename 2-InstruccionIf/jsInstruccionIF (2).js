a45da13ea64b91000d28ad9e0e3b5d270325b4bc
aguschossovov <47614128+aguschossovov@users.noreply.github.com>
2019-02-13 22:27:15 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -1,6 +1,18 @@
 function mostrar()
 {
+
 //tomo la edad  
+
+	var edad
+
+	edad = document.getElementById("edad").value 
+
+	if(edad>17) {
+		
+
+	alert("Usted es mayor de edad");
+
+	}
 
 
 
--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,6 +1,21 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
+
+		edad = document.getElementById("edad").value;
+
+	if(edad>=13 && edad<=17) {
+
+		
+
+
+
+
+
+
+
+
 
 
 
